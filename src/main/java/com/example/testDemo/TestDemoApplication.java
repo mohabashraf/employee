0b2b6e9640836abb8651535a814dc20d05fcdde5
mohabashraf <mohabashraf466@gmail.com>
2020-01-08 14:34:00 +0200

    add configuration and printer

--- src/main/java/com/example/testDemo/TestDemoApplication.java
+++ src/main/java/com/example/testDemo/TestDemoApplication.java
@@ -2,17 +2,48 @@ package com.example.testDemo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class TestDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestDemoApplication.class, args);
+	}
+
+	@Configuration
+	public class Myconfig{
+
+		@Bean
+		EmployeeFetcher employeeFetcher(){
+			return  new EmployeeFetcher();
+		}
+
+		@Bean
+		EmployeePrinter employeePrinter(){
+			return new EmployeePrinter();
+		}
+
+	}
+
+	class EmployeePrinter{
+
+		@Autowired
+		private EmployeeFetcher employeeFetcher;
+
+		String print(){
+			return employeeFetcher.fetchEmployees().
+					stream().map(employee -> employee.name).
+					collect(Collectors.joining("/n"));
+		}
 	}
 
 	class EmployeeFetcher{
